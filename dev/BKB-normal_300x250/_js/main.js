f95767998305ade373d9725c173845c12e7bf47a
Gar <gar@lonelydatum.com>
2022-06-08 13:44:39 -0400

    ON

---
+++ dev/BKB-normal_300x250/_js/main.js
@@ -0,0 +1,46 @@
+
+
+
+import {init, olg, size, chev, bbSecond, bb2, cluserF} from '../../_common/js/common.js'
+
+
+
+function bb_a(){
+	
+
+
+	return tl
+}
+
+
+
+function start(){
+	const tl = init()
+	tl.add(cluserF())
+
+
+
+	tl.to([".o-shadow", ".proline", ".o"], {duration:.1, opacity:0}, "+=.1")
+	tl.from(".bring",  {duration:.25, x:"-=100", opacity:0})
+	tl.add(chev())
+
+
+
+	
+	tl.from(".proline-end",  {duration:.3, opacity:0}, "+=.1")
+	
+
+
+	
+	tl.add(bb2())
+
+}
+
+
+
+start()
+
+
+
+module.exports = {};
+
---
+++ dev/BKB-normal_300x600/_js/main.js
@@ -0,0 +1,38 @@
+
+
+
+import {init, olg, size, chev, bbSecond, bb2, cluserF} from '../../_common/js/common.js'
+
+
+
+function start(){
+	const tl = init()
+	tl.add(cluserF())
+
+
+	tl.to([".o-shadow", ".proline", ".o"], {duration:.1, opacity:0}, "+=.1")
+	tl.from(".bring",  {duration:.25, x:"-=100", opacity:0})
+	tl.add(chev())
+
+
+
+	
+	tl.to(".chev_1a", {duration:.3, opacity:0}, "+=.1")
+	tl.from(".proline-end",  {duration:.3, opacity:0}, "+=.1")
+	tl.to(".chev_1b", {duration:.3, opacity:0}, "+=.3")
+	
+
+
+	
+	tl.add(bb2())
+
+}
+
+
+
+start()
+
+
+
+module.exports = {};
+
